38408a81c489e339dab1d7d9dfc858dfb2a1bfe4
clowns123 <clowns1232@gmail.com>
2021-02-06 22:35:22 +0900

    feat : front router setting 라우터 폴더 구조 및 app.js에 적용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
+import { ConnectedRouter } from "connected-react-router";
+import Router from "./routers/Router";
 
 const App = () => {
-  return <div>test</div>;
+  return (
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Router />
+      </ConnectedRouter>
+    </Provider>
+  );
 };
 
 export default App;
---
+++ frontend/src/routers/Router.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const Router = () => {
+  return (
+    <>
+      <Header />
+      <h1>My Blog</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Router;
